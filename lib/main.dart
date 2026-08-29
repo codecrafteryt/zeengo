eeb85431c9c7337e8d5caaed7971e115b9f9cec8
chsalman <ch.ameersalman@gmail.com>
2026-08-29 15:38:41 +0500

    ✨ Initialize Firebase and Notification Service:
    - Initialize `Firebase` using `DefaultFirebaseOptions` in the `main` function.
    - Integrate `NotificationService` by calling its `init()` method during the application startup sequence.
    - Add necessary imports for `firebase_core`, `firebase_options.dart`, and `NotificationService` to `main.dart`.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -8,14 +9,20 @@ import 'controller/language_controller.dart';
 import 'controller/theme_controller.dart';
 import 'data/helper/get_di.dart';
 import 'data/languages.dart';
+import 'firebase_options.dart';
+import 'services/notification_service.dart';
 import 'services/stripe_payment_service.dart';
 import 'utils/values/app_theme.dart';
 import 'views/screen/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await dotenv.load(fileName: '.env');
   await DependencyInjection.init();
+  await NotificationService.instance.init();
   await StripePaymentService.instance.init();
   runApp(const MyApp());
 }
